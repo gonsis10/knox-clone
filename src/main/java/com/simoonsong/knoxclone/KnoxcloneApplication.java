73fd067a0aece9875fa8e4f3f3a4369ee5e1ca52
Simoon Song <simoonsong1006@gmail.com>
2025-10-10 12:28:40 -0700

    init commit

---
+++ src/main/java/com/simoonsong/knoxclone/KnoxcloneApplication.java
@@ -0,0 +1,13 @@
+package com.simoonsong.knoxclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnoxcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnoxcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simoonsong/knoxclone/KnoxcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simoonsong.knoxclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnoxcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
